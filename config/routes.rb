217568d4012fbb6cdc980edcc28f4442d985bcfe
Petar Ivacic <petar.ivacic@temple.edu>
2013-10-31 23:23:54 -0400

    Fix Git

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blog::Application.routes.draw do
   root 'home#resume'
+  #get ''
   #get "home/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

e9e4d12107761f999f64c955a0f4d502a31695d0
Petar Ivacic <petar.ivacic@temple.edu>
2013-10-31 23:54:40 -0400

    Working on Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Blog::Application.routes.draw do
+  resources :posts
   root 'home#resume'
-  #get ''
+
   #get "home/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

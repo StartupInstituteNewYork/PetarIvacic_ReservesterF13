17a8a6fa7c90512e82151aeb6232947eadce0995
Petar Ivacic <petar.ivacic@temple.edu>
2013-11-04 20:03:43 -0500

    Create Restaurant Controller + Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101023429) do
+ActiveRecord::Schema.define(version: 20131105005825) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
